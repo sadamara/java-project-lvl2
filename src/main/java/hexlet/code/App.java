3e3310cef3c13eef700fd30c84d06fa2508b1d19
sadamara <andrewgalkin0303@gmail.com>
2022-09-19 13:14:35 +0300

    step 5 passed

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,42 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<String> {
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
+
+    @Override
+    public String call() throws Exception {
+        String diff = Differ.generate(filepath1, filepath2);
+        System.out.println(diff);
+        return null;
+    }
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]", paramLabel = "format")
+    private String format;
+
+
+
+
 }
+
+
---
+++ src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,57 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Differ {
+    public static String generate(String firstPath, String secondPath) throws IOException {
+        String fileText1 = Files.readString(Path.of(firstPath));
+        String fileText2 = Files.readString(Path.of(secondPath));
+        Map<String, Object> map1 = parse(fileText1);
+        Map<String, Object> map2 = parse(fileText2);
+        Set<String> allKeys = getAllKeys(map1, map2);
+        StringBuilder result = new StringBuilder("{\n");
+        for (String key : allKeys) {
+            if(map1.containsKey(key) && !map2.containsKey(key)) {
+                result.append("  - " + key + ": " + map1.get(key) + "\n");
+            }
+            else if(!map1.containsKey(key) && map2.containsKey(key)) {
+                result.append("  + " + key + ": " + map2.get(key) + "\n");
+            }
+            else if(map1.containsKey(key) && map2.containsKey(key)) {
+                if (map1.get(key).equals(map2.get(key))){
+                    result.append("    " + key + ": " + map1.get(key) + "\n");
+                }
+                else {
+                    result.append("  - " + key + ": " + map1.get(key) + "\n");
+                    result.append("  + " + key + ": " + map2.get(key) + "\n");
+                }
+            }
+        }
+        result.append("}");
+        return result.toString();
+        }
+
+
+    public static Map<String, Object> parse(String string) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map
+                = objectMapper.readValue(string, new TypeReference<Map<String,Object>>(){});
+        return map;
+    }
+
+    public static Set<String> getAllKeys (Map<String, Object> map1, Map<String, Object> map2) {
+        TreeSet<String> allKeys = new TreeSet<>(map1.keySet());
+        allKeys.addAll(map2.keySet());
+        return allKeys;
+
+    }
+}
